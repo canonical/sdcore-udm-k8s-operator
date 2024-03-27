9d67ba924226c927a8353ea1d7c408d4c64cd6ba
Patricia Reinoso <patricia.reinoso@canonical.com>
2024-03-27 09:33:52 +0100

    fix: remove unnecessary callback when relation broken (#91)

--- src/charm.py
+++ src/charm.py
@@ -76,7 +76,6 @@ class UDMOperatorCharm(CharmBase):
         self.framework.observe(self.on.udm_pebble_ready, self._configure_sdcore_udm)
         self.framework.observe(self.on.fiveg_nrf_relation_joined, self._configure_sdcore_udm)
         self.framework.observe(self._nrf_requires.on.nrf_available, self._configure_sdcore_udm)
-        self.framework.observe(self._nrf_requires.on.nrf_broken, self._configure_sdcore_udm)
         self.framework.observe(self.on.certificates_relation_joined, self._configure_sdcore_udm)
         self.framework.observe(
             self.on.certificates_relation_broken, self._on_certificates_relation_broken
