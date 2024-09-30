9ca4483e90111a140737c0957d101f861a54d6c5
guillaume <guillaume.belanger27@gmail.com>
2024-09-30 11:58:17 -0400

    chore: bump scenario to 7.x
    Signed-off-by: guillaume <guillaume.belanger27@gmail.com>

--- tests/unit/test_charm_get_home_network_public_key_action.py
+++ tests/unit/test_charm_get_home_network_public_key_action.py
@@ -1,10 +1,11 @@
 # Copyright 2024 Canonical Ltd.
 # See LICENSE file for licensing details.
 
-
 import tempfile
 
+import pytest
 import scenario
+from ops.testing import ActionFailed
 
 from tests.unit.fixtures import UDMUnitTestFixtures
 
@@ -19,20 +20,17 @@ class TestCharmGetHomeNetworkPublicKeyAction(UDMUnitTestFixtures):
             leader=True,
             containers=[container],
         )
-        action = scenario.Action(
-            name="get-home-network-public-key",
-        )
 
-        action_output = self.ctx.run_action(action, state_in)
+        with pytest.raises(ActionFailed) as exc_info:
+            self.ctx.run(self.ctx.on.action("get-home-network-public-key"), state_in)
 
-        assert action_output.success is False
-        assert action_output.failure == "Container is not ready yet."
+        assert exc_info.value.message == "Container is not ready yet."
 
     def test_given_key_not_stored_when_get_home_network_public_key_action_then_event_fails(self):
         with tempfile.TemporaryDirectory() as temp_dir:
             certs_mount = scenario.Mount(
                 location="/support/TLS",
-                src=temp_dir,
+                source=temp_dir,
             )
             container = scenario.Container(
                 name="udm",
@@ -43,20 +41,17 @@ class TestCharmGetHomeNetworkPublicKeyAction(UDMUnitTestFixtures):
                 leader=True,
                 containers=[container],
             )
-            action = scenario.Action(
-                name="get-home-network-public-key",
-            )
 
-            action_output = self.ctx.run_action(action, state_in)
+            with pytest.raises(ActionFailed) as exc_info:
+                self.ctx.run(self.ctx.on.action("get-home-network-public-key"), state_in)
 
-            assert action_output.success is False
-            assert action_output.failure == "Home network private key is not stored yet."
+            assert exc_info.value.message == "Home network private key is not stored yet."
 
     def test_given_stored_when_get_home_network_public_key_action_then_key_returned(self):
         with tempfile.TemporaryDirectory() as temp_dir:
             config_mount = scenario.Mount(
                 location="/etc/udm/",
-                src=temp_dir,
+                source=temp_dir,
             )
             container = scenario.Container(
                 name="udm",
@@ -67,14 +62,10 @@ class TestCharmGetHomeNetworkPublicKeyAction(UDMUnitTestFixtures):
                 leader=True,
                 containers=[container],
             )
-            action = scenario.Action(
-                name="get-home-network-public-key",
-            )
             with open(f"{temp_dir}/home_network.key", "w") as f:
                 f.write("f0179dd5c4c8ca35557d21e70f770c247abc38382da1ed6cc42fbf89f3644d65")
 
-            action_output = self.ctx.run_action(action, state_in)
+            self.ctx.run(self.ctx.on.action("get-home-network-public-key"), state_in)
 
-            assert action_output.success is True
-            assert action_output.results
-            assert "public-key" in action_output.results
+            assert self.ctx.action_results
+            assert self.ctx.action_results["public-key"] is not None
