d96feecf84d23719b5d4ecffee544401e31c48d1
Ghislain Bourgeois <ghislain.bourgeois@canonical.com>
2024-10-02 16:18:16 -0400

    chore: Refactor the Terraform module to follow spec CC006

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,10 +1,10 @@
 # Copyright 2024 Canonical Ltd.
 # See LICENSE file for licensing details.
 
-variable "model_name" {
-  description = "Name of Juju model to deploy application to."
+variable "app_name" {
+  description = "Name of the application in the Juju model."
   type        = string
-  default     = ""
+  default     = "udm"
 }
 
 variable "channel" {
@@ -13,8 +13,44 @@ variable "channel" {
   default     = "1.5/edge"
 }
 
-variable "app_name" {
-  description = "Name of the application in the Juju model."
+variable "config" {
+  description = "Application config. Details about available options can be found at https://charmhub.io/sdcore-udm-k8s-operator/configure."
+  type        = map(string)
+  default     = {}
+}
+
+variable "constraints" {
+  description = "Juju constraints to apply for this application."
   type        = string
-  default     = "udm"
+  default     = "arch=amd64"
+}
+
+variable "model" {
+  description = "Reference to a `juju_model`."
+  type        = string
+  default     = ""
+}
+
+variable "resources" {
+  description = "Resources to use with the application. Details about available options can be found at https://charmhub.io/sdcore-udm-k8s-operator/configure."
+  type        = map(string)
+  default     = {}
+}
+
+variable "revision" {
+  description = "Revision number of the charm"
+  type        = number
+  default     = null
+}
+
+variable "units" {
+  description = "Number of units to deploy"
+  type        = number
+  default     = 1
+
+  validation {
+    condition     = var.units == 1
+    error_message = "Scaling is not supported for this charm."
+  }
+
 }
