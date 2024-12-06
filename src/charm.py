00774952d19a8595a62d246b088ed687c03e5baa
Guillaume Belanger <guillaume.belanger27@gmail.com>
2024-12-06 13:57:22 -0500

    chore: remove debug level env vars
    Signed-off-by: Guillaume Belanger <guillaume.belanger27@gmail.com>

--- src/charm.py
+++ src/charm.py
@@ -645,10 +645,6 @@ class UDMOperatorCharm(CharmBase):
     @property
     def _environment_variables(self) -> dict:
         return {
-            "GRPC_GO_LOG_VERBOSITY_LEVEL": "99",
-            "GRPC_GO_LOG_SEVERITY_LEVEL": "info",
-            "GRPC_TRACE": "all",
-            "GRPC_VERBOSITY": "debug",
             "POD_IP": _get_pod_ip(),
             "MANAGED_BY_CONFIG_POD": "true",
         }
