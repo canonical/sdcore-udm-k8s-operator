5ad46545568339a60a3e80cd590cda9f34c2ba78
Guillaume Belanger <guillaume.belanger@canonical.com>
2024-12-09 09:17:50 -0500

    feat: add log-level configuration option (#366)
    Signed-off-by: Guillaume Belanger <guillaume.belanger27@gmail.com>

--- tests/unit/test_charm_configure.py
+++ tests/unit/test_charm_configure.py
@@ -184,10 +184,6 @@ class TestCharmConfigure(UDMUnitTestFixtures):
                                 "override": "replace",
                                 "command": "/bin/udm --udmcfg /etc/udm/udmcfg.yaml",
                                 "environment": {
-                                    "GRPC_GO_LOG_VERBOSITY_LEVEL": "99",
-                                    "GRPC_GO_LOG_SEVERITY_LEVEL": "info",
-                                    "GRPC_TRACE": "all",
-                                    "GRPC_VERBOSITY": "debug",
                                     "POD_IP": "1.1.1.1",
                                     "MANAGED_BY_CONFIG_POD": "true",
                                 },
